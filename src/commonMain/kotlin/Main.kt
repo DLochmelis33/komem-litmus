091e1fe1855610d19c79a6b4c3eb7d6bf08de2f3
Evgenii Moiseenko <evg.moiseenko94@gmail.com>
2023-07-07 18:58:02 +0200

    trying to get rid of boxing by passing data holder to threads as an argument
    * TODO: check that generics do not introduce heavy-weight type-casts machinery that interfere with the litmus test

--- src/commonMain/kotlin/Main.kt
+++ src/commonMain/kotlin/Main.kt
@@ -1,43 +1,43 @@
-import komem.litmus.RunningParams
-import komem.litmus.barriers.SpinBarrier
-import komem.litmus.getOutcomeSetup
-import komem.litmus.groupToInfo
-import komem.litmus.prettyPrint
-import komem.litmus.runners.WorkerTestRunner
-import komem.litmus.tests.SBTest
+import komem.litmus.dslTest.*
 import kotlin.time.measureTime
 
 fun main() {
 
-//    val sb = litmusTest {
-//        var x = 0
-//        var y = 0
-//        thread {
-//            x = 1
-//            o1 = y
-//        }
-//        thread {
-//            y = 1
-//            o2 = x
-//        }
-//    }
-//    measureTime {
-//        sb.run(10_000_000)
-//    }.let { println("${it.inWholeSeconds} seconds") }
+    class Data {
+        var x = 0
+        var y = 0
+    }
 
-    val runner = WorkerTestRunner
+    val sb = litmusTest<Data> {
+        init {
+            Data()
+        }
+        thread {
+            x = 1
+            o1 = y
+        }
+        thread {
+            y = 1
+            o2 = x
+        }
+    }
     measureTime {
-        runner.runTest(
-            RunningParams(
-                10_000_000,
-                10,
-                null,
-                null,
-                ::SpinBarrier
-            ),
-            ::SBTest
-        ).groupToInfo(SBTest().getOutcomeSetup()).prettyPrint()
-    }.let { println("and ${it.inWholeSeconds} seconds") }
+        sb.run(10_000)
+    }.let { println("${it.inWholeSeconds} seconds") }
+
+//    val runner = WorkerTestRunner
+//    measureTime {
+//        runner.runTest(
+//            RunningParams(
+//                10_000_000,
+//                10,
+//                null,
+//                null,
+//                ::SpinBarrier
+//            ),
+//            ::SBTest
+//        ).groupToInfo(SBTest().getOutcomeSetup()).prettyPrint()
+//    }.let { println("and ${it.inWholeSeconds} seconds") }
 
 //    val paramsList = variateParams(
 //        batchSizeSchedule = listOf(100_000),
